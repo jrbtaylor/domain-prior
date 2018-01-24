cd0592304650f846cc06f897805deac94ef5ff34
jrbtaylor <jasonrbtaylor@gmail.com>
2018-01-24 17:56:56 -0500

    add EMNIST to data

--- data.py
+++ data.py
@@ -3,33 +3,15 @@ Written by Jason Taylor <jasonrbtaylor@gmail.com> 2018-2019
 """
 
 import os
-import random
 
-import numpy as np
 import torch
 from torchvision import datasets, transforms
+from torchvision.datasets.mnist import read_label_file, read_image_file
 
 
-def onehot(n_classes):
-    def onehot_fcn(x):
-        y = np.zeros((n_classes), dtype='float32')
-        y[x] = 1
-        return y
-    return onehot_fcn
-
-
-def augment(img_size=28,scale=0.1):
-    if random.random()<0.5:  # random crop & upsampling
-            resize = transforms.RandomResizedCrop(
-                img_size, scale=(1-scale,1.0), ratio=(1-scale,1/(1-scale)))
-    else:  # random pad & downsampling
-        left = random.randint(0,int(np.round(scale*img_size)))
-        right = random.randint(0,int(np.round(scale*img_size)-left))
-        top = random.randint(0,int(np.round(scale*img_size)))
-        bottom = random.randint(0, int(np.round(scale*img_size)-top))
-        resize = transforms.Compose([transforms.Pad((left,top,right,bottom)),
-                                     transforms.Resize((img_size,img_size))])
-    return transforms.Compose([resize,transforms.ToTensor()])
+def augment(rotate=5):
+    return transforms.Compose([transforms.RandomRotation(rotate),
+                               transforms.ToTensor()])
 
 
 def loader(dataset, batch_size, n_workers=8):
@@ -41,24 +23,124 @@ def loader(dataset, batch_size, n_workers=8):
     datapath = os.path.join(os.getenv('HOME'), 'data', dataset.lower())
     dataset_args = {'root':datapath,
                     'download':True,
-                    'transform':augment()}
+                    'transform':transforms.ToTensor()}
 
     if dataset.lower()=='mnist':
         dataset_init = datasets.MNIST
         n_classes = 10
     elif dataset.lower()=='emnist':
-        dataset_init = datasets.eMNIST
-        n_classes = 62
-        dataset_args.update({'split':'byclass'})
+        dataset_init = EMNIST
+        n_classes = 37
+        dataset_args.update({'split':'letters'})
     else:
         dataset_init = datasets.FashionMNIST
         n_classes = 10
-    onehot_fcn = onehot(n_classes)
-    dataset_args.update({'target_transform':onehot_fcn})
 
-    train_loader = torch.utils.data.DataLoader(
-        dataset_init(train=True, **dataset_args), shuffle=True, **loader_args)
     val_loader = torch.utils.data.DataLoader(
         dataset_init(train=False, **dataset_args), shuffle=False, **loader_args)
 
-    return train_loader, val_loader
+    dataset_args['transform'] = augment()
+    train_loader = torch.utils.data.DataLoader(
+        dataset_init(train=True, **dataset_args), shuffle=True, **loader_args)
+
+    return train_loader, val_loader, n_classes
+
+
+
+# Note: Can't build master ver of pytorch/torchvision, so copied this here
+class EMNIST(datasets.MNIST):
+    """`EMNIST <https://www.nist.gov/itl/iad/image-group/emnist-dataset/>`_ Dataset.
+    Args:
+        root (string): Root directory of dataset where ``processed/training.pt``
+            and  ``processed/test.pt`` exist.
+        split (string): The dataset has 6 different splits: ``byclass``, ``bymerge``,
+            ``balanced``, ``letters``, ``digits`` and ``mnist``. This argument specifies
+            which one to use.
+        train (bool, optional): If True, creates dataset from ``training.pt``,
+            otherwise from ``test.pt``.
+        download (bool, optional): If true, downloads the dataset from the internet and
+            puts it in root directory. If dataset is already downloaded, it is not
+            downloaded again.
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.RandomCrop``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+    """
+    url = 'http://biometrics.nist.gov/cs_links/EMNIST/gzip.zip'
+    splits = ('byclass', 'bymerge', 'balanced', 'letters', 'digits', 'mnist')
+
+    def __init__(self, root, split, **kwargs):
+        if split not in self.splits:
+            raise ValueError('Split "{}" not found. Valid splits are: {}'.format(
+                split, ', '.join(self.splits),
+            ))
+        self.split = split
+        self.training_file = self._training_file(split)
+        self.test_file = self._test_file(split)
+        super(EMNIST, self).__init__(root, **kwargs)
+
+    def _training_file(self, split):
+        return 'training_{}.pt'.format(split)
+
+    def _test_file(self, split):
+        return 'test_{}.pt'.format(split)
+
+    def download(self):
+        """Download the EMNIST data if it doesn't exist in processed_folder already."""
+        import errno
+        from six.moves import urllib
+        import gzip
+        import shutil
+        import zipfile
+
+        if self._check_exists():
+            return
+
+        # download files
+        try:
+            os.makedirs(os.path.join(self.root, self.raw_folder))
+            os.makedirs(os.path.join(self.root, self.processed_folder))
+        except OSError as e:
+            if e.errno == errno.EEXIST:
+                pass
+            else:
+                raise
+
+        print('Downloading ' + self.url)
+        data = urllib.request.urlopen(self.url)
+        filename = self.url.rpartition('/')[2]
+        raw_folder = os.path.join(self.root, self.raw_folder)
+        file_path = os.path.join(raw_folder, filename)
+        with open(file_path, 'wb') as f:
+            f.write(data.read())
+
+        print('Extracting zip archive')
+        with zipfile.ZipFile(file_path) as zip_f:
+            zip_f.extractall(raw_folder)
+        os.unlink(file_path)
+        gzip_folder = os.path.join(raw_folder, 'gzip')
+        for gzip_file in os.listdir(gzip_folder):
+            if gzip_file.endswith('.gz'):
+                print('Extracting ' + gzip_file)
+                with open(os.path.join(raw_folder, gzip_file.replace('.gz', '')), 'wb') as out_f, \
+                        gzip.GzipFile(os.path.join(gzip_folder, gzip_file)) as zip_f:
+                    out_f.write(zip_f.read())
+        shutil.rmtree(gzip_folder)
+
+        # process and save as torch files
+        for split in self.splits:
+            print('Processing ' + split)
+            training_set = (
+                read_image_file(os.path.join(raw_folder, 'emnist-{}-train-images-idx3-ubyte'.format(split))),
+                read_label_file(os.path.join(raw_folder, 'emnist-{}-train-labels-idx1-ubyte'.format(split)))
+            )
+            test_set = (
+                read_image_file(os.path.join(raw_folder, 'emnist-{}-test-images-idx3-ubyte'.format(split))),
+                read_label_file(os.path.join(raw_folder, 'emnist-{}-test-labels-idx1-ubyte'.format(split)))
+            )
+            with open(os.path.join(self.root, self.processed_folder, self._training_file(split)), 'wb') as f:
+                torch.save(training_set, f)
+            with open(os.path.join(self.root, self.processed_folder, self._test_file(split)), 'wb') as f:
+                torch.save(test_set, f)
+
+        print('Done!')
